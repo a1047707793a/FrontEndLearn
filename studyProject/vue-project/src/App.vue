ed0b21afc9da773e72b41aaeeef92980bc31c056
a1047707793a <1047707793@qq.com>
2024-07-13 20:51:34 +0800

    learn

--- studyProject/vue-project/src/App.vue
+++ studyProject/vue-project/src/App.vue
@@ -1,16 +1,17 @@
 <template>  <!--定义HTML模板 -->
-  <div id="app">
-    <nav>
-      <router-link to="/">vue Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-    <h1>{{message}}</h1>
+  <div>
+<!--    <h1>{{message}}</h1>-->
+    <element-view>
+
+    </element-view>
   </div>
 </template>
 
 <script>
+  import ElementView from "@/views/Element/elementLearn.vue";
+
   export default {
+    components: {ElementView},
     data(){
       return{
         message:"Hello vue"
@@ -20,24 +21,5 @@
 </script>
 
 <style>  /*定义CSS样式*/
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
